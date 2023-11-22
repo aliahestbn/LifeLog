3d57d7c04dbc53ecb624c8a6bb5875146f6b1b45
aliahestbn <aliah.esteban.cics@ust.edu.ph>
2023-11-22 23:23:01 +0800

    firebase setup done

--- app/src/main/java/com/example/lifelog/MainActivity.kt
+++ app/src/main/java/com/example/lifelog/MainActivity.kt
@@ -4,12 +4,42 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+data class DatabaseClass(   var EmpName : String,
+                            var EmpPosition : String,
+                            var EmpDepartment : String)
 
 class MainActivity : AppCompatActivity() {
+    lateinit var databaseReference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //create
+        databaseReference = FirebaseDatabase.getInstance().getReference("DatabaseName")
+        var databaseClass = DatabaseClass("Park Jihoon", "SSE", "Somewhere")
+
+        var dataKey = databaseReference.push().getKey()
+        databaseReference.child("uniqueid").setValue(databaseClass).addOnSuccessListener {
+            Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+        }
+
+        //update
+        var empUpdate = mapOf<String, String>("empName" to "Jakob Michael Palomo")
+        databaseReference.child("uniqueid").child("sampleOne").updateChildren(empUpdate).addOnSuccessListener {
+            Toast.makeText(this, "Update", Toast.LENGTH_SHORT).show()
+        }
+
+        //remove
+        databaseReference.child("uniqueid").child("sampleOne").removeValue().addOnSuccessListener {
+            Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show()
+        }
     }
+
+
     fun redirectionfun(view: View) { //Main > Login
         val intent = Intent(this, Login::class.java);
         startActivity(intent);
@@ -18,4 +48,5 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, Register::class.java);
         startActivity(intent);
     }
+
 }
