ca63ee29fd9f0eaef1241afe9b1fd38542551453
YnaSophiaDR <ynasophia.delrosario.cics@ust.edu.ph>
2023-11-30 09:24:29 +0800

    register and login

--- app/src/main/java/com/example/lifelog/Login.kt
+++ app/src/main/java/com/example/lifelog/Login.kt
@@ -3,16 +3,45 @@ package com.example.lifelog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.lifelog.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity(){
+
+    private lateinit var binding:ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-    }
-    fun redirectToHomepage(view: View) { //Login > HomePage
-        val intent = Intent(this, HomePage::class.java);
-        startActivity(intent);
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.btnLoginUser.setOnClickListener{
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnLoginUser.setOnClickListener{
+            val username = binding.txtUsername.text.toString()
+            val pass = binding.txtPassword.text.toString()
+
+            if (username.isNotEmpty() && pass.isNotEmpty()){
+                    firebaseAuth.signInWithEmailAndPassword(username, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, HomePage::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }else{
+                Toast.makeText(this, "Empty Fields are not Allowed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 }
+
+//validation for error: user is not registered, wrong password
--- app/src/main/java/com/example/lifelog/Register.kt
+++ app/src/main/java/com/example/lifelog/Register.kt
@@ -3,16 +3,49 @@ package com.example.lifelog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.lifelog.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Register : AppCompatActivity(){
+
+    private lateinit var binding:ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-    }
-    fun redirectToLogin (view : View){ //Register > Login
-        val intent= Intent(this, Login::class.java);
-        startActivity(intent);
-    }
 
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.btnRegisterUser.setOnClickListener{
+            val username = binding.txtUsername.text.toString()
+            val pass = binding.txtPassword.text.toString()
+            val confirmPass = binding.txtConfirmPass.text.toString()
+
+            if (username.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()){
+                if (pass == confirmPass){
+                    firebaseAuth.createUserWithEmailAndPassword(username, pass).addOnCompleteListener{
+                        if (it.isSuccessful){
+                            val intent = Intent(this, Login::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }else{
+                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+
+                }
+            }else{
+                Toast.makeText(this, "Empty Fields are not Allowed", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
 }
+
+//validation pag existing na yung username sa database
